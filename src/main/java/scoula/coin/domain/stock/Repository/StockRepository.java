65126cd7d7f448eada141015220ff7768ede64d9
singingsandhill <jisu0259@naver.com>
2025-01-26 00:05:09 +0900

    Feat: 주식 기능 기초 설계

--- src/main/java/scoula/coin/domain/stock/Repository/StockRepository.java
+++ src/main/java/scoula/coin/domain/stock/Repository/StockRepository.java
@@ -1,7 +1,8 @@
 package scoula.coin.domain.stock.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockRepository {
+public interface StockRepository extends JpaRepository {
 }
--- src/main/java/scoula/coin/domain/stock/Service/StockService.java
+++ src/main/java/scoula/coin/domain/stock/Service/StockService.java
@@ -3,10 +3,15 @@ package scoula.coin.domain.stock.Service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import scoula.coin.application.dto.ApiResponse;
 
 @Service
 public class StockService {
 
     @Value("${mystock.app_key}")
     private String appKey;
+
+    public ApiResponse getValance(String symbol) {
+        return null;
+    }
 }
