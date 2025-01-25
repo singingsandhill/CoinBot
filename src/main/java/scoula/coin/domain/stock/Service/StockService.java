9a94a49b4d7384b5043ce84a5f0e31ad2fca4944
singingsandhill <jisu0259@naver.com>
2025-01-25 14:23:47 +0900

    Feat: 주식 서비스 기본 설계

--- src/main/java/scoula/coin/domain/stock/Service/StockService.java
+++ src/main/java/scoula/coin/domain/stock/Service/StockService.java
@@ -1,7 +1,12 @@
 package scoula.coin.domain.stock.Service;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StockService {
+
+    @Value("${mystock.app_key}")
+    private String appKey;
 }
